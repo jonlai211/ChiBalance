d03fa2017cbc408616607ac415258ec7510b549c
ThomasC86 <holok806@gmail.com>
2024-09-21 22:48:23 -0700

    heartbeat

--- hardware_server/app.py
+++ hardware_server/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/heartbeat')
+def heartbeat():
+    return 'Heartbeat'
+
 if __name__ == '__main__':
     app.run(debug=True)
