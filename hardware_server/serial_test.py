3355cc9d64d07b4c4efbb783fc348ab31b31681a
ThomasC86 <holok806@gmail.com>
2024-09-21 22:45:52 -0700

    test works with arduino uno

---
+++ hardware_server/serial_test.py
@@ -0,0 +1,12 @@
+import serial
+import os
+import dotenv
+import time
+dotenv.load_dotenv()
+arduino_serial = os.getenv('ARDUINO_SERIAL')
+
+ser = serial.Serial(arduino_serial, 115200)
+
+while True:
+    print(ser.readline())
+    time.sleep(1)
